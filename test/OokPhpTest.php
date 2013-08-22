3a4501e5188387064ae0ce87c96ccc85978579b5
Sam Smith <git@samsmith.io>
2013-08-22 22:08:45 +0100

    Move the test fixtures into their own directory.

--- test/OokPhpTest.php
+++ test/OokPhpTest.php
@@ -15,68 +15,68 @@ class OokPhpTest extends PHPUnit_Framework_TestCase
 {
     public function test_it_should_fail_when_the_file_doesnt_exist()
     {
-        $process = $this->runOokPhp('file-doesnt-exist.ook');
+        $process = $this->runOokPhp('fixtures/file-doesnt-exist.ook');
         $this->assertFalse($process->isSuccessful());
     }
 
     public function test_it_should_message_the_user_when_the_file_doesnt_exist()
     {
-        $process = $this->runOokPhp('file-doesnt-exist.ook');
+        $process = $this->runOokPhp('fixtures/file-doesnt-exist.ook');
         $this->assertRegExp('/^No such file -- .*$/', $process->getErrorOutput());
     }
 
     public function test_it_should_succeed_when_the_file_is_empty()
     {
-        $process = $this->runOokPhp('file-is-empty.ook');
+        $process = $this->runOokPhp('fixtures/file-is-empty.ook');
         $this->assertTrue($process->isSuccessful());
     }
 
     public function test_it_should_fail_when_the_file_contains_an_odd_number_of_ooks()
     {
-        $process = $this->runOokPhp('contains-an-odd-number-of-ooks.ook');
+        $process = $this->runOokPhp('fixtures/contains-an-odd-number-of-ooks.ook');
         $this->assertFalse($process->isSuccessful());
     }
 
     public function test_it_should_message_the_user_when_the_file_contains_an_odd_number_of_ooks()
     {
-        $process = $this->runOokPhp('contains-an-odd-number-of-ooks.ook');
+        $process = $this->runOokPhp('fixtures/contains-an-odd-number-of-ooks.ook');
         $this->assertRegExp('/^.*\: Syntax error\: expected Ook\., Ook\?, or Ook\!$/', $process->getErrorOutput());
     }
 
     public function test_it_should_succeed_when_the_file_doesnt_contain_any_ooks()
     {
-        $process = $this->runOokPhp('file-doesnt-contain-any-ooks.ook');
+        $process = $this->runOokPhp('fixtures/file-doesnt-contain-any-ooks.ook');
         $this->assertTrue($process->isSuccessful());
     }
 
     public function test_it_should_successfully_execute_hello_world_ook()
     {
-        $process = $this->runOokPhp('hello-world.ook');
+        $process = $this->runOokPhp('fixtures/hello-world.ook');
         $this->assertTrue($process->isSuccessful());
         $this->assertEquals('Hello World!', $process->getOutput());
     }
 
     public function test_it_should_fail_when_the_file_contains_an_unbalanced_bang_question()
     {
-        $process = $this->runOokPhp('contains-an-unbalanced-bang-question.ook');
+        $process = $this->runOokPhp('fixtures/contains-an-unbalanced-bang-question.ook');
         $this->assertFalse($process->isSuccessful());
     }
 
     public function test_it_should_message_the_user_when_the_file_contains_an_unbalanced_bang_question()
     {
-        $process = $this->runOokPhp('contains-an-unbalanced-bang-question.ook');
+        $process = $this->runOokPhp('fixtures/contains-an-unbalanced-bang-question.ook');
         $this->assertRegExp('/^.*\: Syntax error\: unbalanced Ook\! Ook\?$/', $process->getErrorOutput());
     }
 
     public function test_it_should_fail_when_the_file_contains_a_question_bang_before_a_bang_question()
     {
-        $process = $this->runOokPhp('contains-a-question-bang-before-a-bang-question.ook');
+        $process = $this->runOokPhp('fixtures/contains-a-question-bang-before-a-bang-question.ook');
         $this->assertFalse($process->isSuccessful());
     }
 
     public function test_it_should_message_the_user_when_the_file_contains_a_question_bang_before_a_bang_question()
     {
-        $process = $this->runOokPhp('contains-a-question-bang-before-a-bang-question.ook');
+        $process = $this->runOokPhp('fixtures/contains-a-question-bang-before-a-bang-question.ook');
         $this->assertRegExp('/^.*\: Syntax error\: unexpected Ook\? Ook\!$/', $process->getErrorOutput());
     }
 
